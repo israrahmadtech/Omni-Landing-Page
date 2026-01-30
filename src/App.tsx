4c569bc03d4b5499906be5d1d7d0ee42c8b393fa
israrahmadtech <israrahmadtech@gmail.com>
2026-01-30 17:26:24 +0500

    another bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Landing from "./pages/Landing"
 import OmniLanding from "./pages/OmniLanding"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/landing" element={<OmniLanding />} />
+        <Route path="/" element={<OmniLanding />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
